728a2f9f237a3622d55e37668e3324599e4783bf
liqiye <1159140456@qq.com>
2019-04-27 14:52:32 +0800

    zuul网关读取config配置中心的配置文件，实现动态获取接口对应网关分配的项目

--- zuul/src/main/java/com/liqiye/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/liqiye/zuul/ZuulApplication.java
@@ -2,8 +2,11 @@ package com.liqiye.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,6 +15,13 @@ public class ZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    // 作用：zuul配置能够使用config分布式配置中心
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    public ZuulProperties zuulProperties(){
+        return new ZuulProperties();
     }
 
 }
